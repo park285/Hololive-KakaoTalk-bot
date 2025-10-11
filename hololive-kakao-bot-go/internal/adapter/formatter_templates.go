a05c4b8013c55046f0bf9a05c10fc6206f66d540
kapu <pjoonwoo111@gmail.com>
2025-10-11 09:26:30 +0000

    Revert "feat: indent alarm next stream details"
    This reverts commit f61633ee0aa23c9794ec029d979356ebdacbdcea.

--- hololive-kakao-bot-go/internal/adapter/formatter_templates.go
+++ hololive-kakao-bot-go/internal/adapter/formatter_templates.go
@@ -20,23 +20,6 @@ func executeFormatterTemplate(name string, data any) (string, error) {
 	formatterOnce.Do(func() {
 		funcMap := template.FuncMap{
 			"add": func(a, b int) int { return a + b },
-			"indent": func(spaces int, text string) string {
-				trimmed := strings.TrimSpace(text)
-				if trimmed == "" {
-					return ""
-				}
-
-				indent := strings.Repeat(" ", spaces)
-				lines := strings.Split(text, "\n")
-				for i, line := range lines {
-					if strings.TrimSpace(line) == "" {
-						lines[i] = indent
-						continue
-					}
-					lines[i] = indent + line
-				}
-				return strings.Join(lines, "\n")
-			},
 		}
 		tmpl := template.New("formatter").Funcs(funcMap)
 		formatterTemplates, formatterErr = tmpl.ParseFS(formatterTemplateFS, "templates/*.tmpl")
